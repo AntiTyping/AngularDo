75c837baf57525d915bd3bb2cdc295ed56a4d25a
Andy Pliszka <andy@isp44.com>
2013-11-04 22:06:48 -0500

    Initial import

---
+++ run-unit-tests.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+karma start config/karma.conf.js --no-single-run --auto-watch
---
+++ test/spec/directives.js
@@ -0,0 +1,10 @@
+'use strict';
+
+describe('Directives', function () {
+
+   beforeEach(module('AngularDoApp'));
+
+   it("should be true", function() {
+     expect(true).toEqual(true);
+   });
+});
