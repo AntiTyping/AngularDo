e99e1fc0d3052a58afbfdf0c01103e04982ab6f9
Andy Pliszka <andy@isp44.com>
2013-11-04 22:48:36 -0500

    Failing TaskCtrl scope test

--- test/spec/controllers.js
+++ test/spec/controllers.js
@@ -7,4 +7,12 @@ describe('Controllers', function () {
    it("should be true", function() {
      expect(true).toEqual(true);
    });
+
+   describe("TaskCtrl", function() {
+     it('should populate scope with list of tasks', inject(function ($controller, $rootScope) {
+       var scope = $rootScope.$new();
+       $controller('TaskCtrl', { $scope: scope });
+       expect(scope.tasks.length).toEqual(3);
+     }));
+   });
 });
