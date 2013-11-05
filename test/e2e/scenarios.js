f156b9a68dc1b3881a9f047308b5f302ad10f818
Andy Pliszka <andy@isp44.com>
2013-11-04 22:20:03 -0500

    Adds Display list of tasks scenario

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('Scenarios', function() {
+describe('Scenarios:', function() {
   beforeEach(function() {
     browser().navigateTo('/proxy/');
   });
@@ -12,6 +12,12 @@ describe('Scenarios', function() {
 
     it("should have About link", function() {
       expect(element("ul.nav li:last").text()).toEqual("About");
+    });
+  });
+
+  describe("Task List", function() {
+    it('should display list of tasks', function() {
+      expect(repeater('tr.task').count()).toBe(3);
     });
   });
 });
