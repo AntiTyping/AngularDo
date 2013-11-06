459aa4d74387b65d5e617506ed804633c75f0e9a
Andy Pliszka <andy@isp44.com>
2013-11-06 02:00:47 -0500

    Adds Pluralize filter implementation

--- app/scripts/filters.js
+++ app/scripts/filters.js
@@ -1,3 +1,12 @@
 'use strict';
 
-angular.module('AngularDoApp');
+angular.module('AngularDoApp')
+  .filter('pluralize', function() {
+    return function(number, noun){
+      if (number == 0)
+        return "No " + noun + "s";
+      if (number == 1)
+        return number + " " + noun;
+      return number + " " + noun + "s";
+    }
+  });
