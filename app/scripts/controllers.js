82a74841845b4da18c946fc51ef6b094e9eb48e7
Andy Pliszka <andy@isp44.com>
2013-11-05 21:37:48 -0500

    Refactors controller to use REST back end

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,21 +1,28 @@
 'use strict';
 
 angular.module('AngularDoApp')
-  .controller('TaskCtrl', function ($scope) {
-    $scope.tasks = [
-      {name: 'Task 1', priority: 'high'},
-      {name: 'Task 2', priority: 'medium'},
-      {name: 'Task 3', priority: 'low'}
-    ];
+  .controller('TaskCtrl', function ($scope, Task, $resource) {
+    $scope.tasks = Task.query();
 
     $scope.add = function(task) {
-      var newTask = new Object();
+      var newTask = new Task();
       newTask.name = task.name;
       newTask.priority = task.priority;
+      newTask.$save();
       $scope.tasks.push(newTask);
     };
 
     $scope.remove = function(index, task) {
+      var id = task.url.replace("http://localhost:3000/tasks/", '');
+      task.$remove({id: id});
       $scope.tasks.splice(index, 1);
     };
-  });
+  })
+  .factory('Task', ['$resource', function($resource){
+    return $resource('http://localhost\\:3000/:path/:id', {}, {
+      query: {method:'GET', params:{path:'tasks.json'}, isArray:true},
+      get: {method:'GET', params:{path:''}},
+      save: {method:'POST', params:{path:'tasks.json'}},
+      remove: {method:'DELETE', params:{path:'tasks'}}
+    });
+  }]);;
