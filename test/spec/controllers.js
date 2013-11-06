bfbc3749168f875ea47c3540fbdb0d8eb3f2c4b8
Andy Pliszka <andy@isp44.com>
2013-11-05 21:36:35 -0500

    Adds REST resource controller test

--- test/spec/controllers.js
+++ test/spec/controllers.js
@@ -9,11 +9,24 @@ describe('Controllers', function () {
    });
 
    describe("TaskCtrl", function() {
-     var TaskCtrl, scope;
+     var TaskCtrl, scope, Task, $save;
 
      beforeEach(inject(function ($controller, $rootScope) {
        scope = $rootScope.$new();
-       TaskCtrl = $controller('TaskCtrl', { $scope: scope });
+       $save = jasmine.createSpy("$save");
+       Task = function Task() {
+         return {
+           $save: $save
+         };
+       };
+       Task.query = function query() {
+         return [
+           {name: 'Task 1', priority: 'high'},
+           {name: 'Task 2', priority: 'medium'},
+           {name: 'Task 3', priority: 'low'}
+         ];
+       };
+       TaskCtrl = $controller('TaskCtrl', { $scope: scope, Task: Task });
      }));
 
      it('should populate scope with list of tasks', function() {
@@ -23,20 +36,40 @@ describe('Controllers', function () {
      describe("add", function() {
        var task;
 
-       it("should adds new task to task list", function() {
+       beforeEach(function() {
          task = {name: 'Task 4', priority: 'high'}
+       });
+
+       it("should adds new task to task list", function() {
          scope.add(task);
          expect(scope.tasks.length).toEqual(4);
          expect(scope.tasks[3].name).toEqual('Task 4');
          expect(scope.tasks[3].priority).toEqual('high');
        });
+
+       it("should save the new task", function() {
+         scope.add(task);
+         expect($save).toHaveBeenCalled();
+       });
      });
 
      describe("remove", function() {
+       var task;
+
+       beforeEach(function() {
+         task = {name: 'Task 4', priority: 'high'}
+         task.$remove = jasmine.createSpy("$remove");
+         task.url = "http://localhost:3000/tasks/111.json";
+       });
+
        it("should remove task to task list", function() {
-         var task = jasmine.createSpy("task");
          scope.remove(1, task);
          expect(scope.tasks.length).toEqual(2);
+       });
+
+       it("should remove new task from data store", function() {
+         scope.remove(1, task);
+         expect(task.$remove).toHaveBeenCalled();
        });
      });
    });
